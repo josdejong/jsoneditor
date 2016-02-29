106c71c0b8e235b39e9a0ee80c306a16773b94d2
Takashi Oguma <bear.mini@gmail.com>
2016-02-29 12:49:15 +0900

    i18n for error messages and add lang option

--- src/js/JSONEditor.js
+++ src/js/JSONEditor.js
@@ -77,7 +77,7 @@ function JSONEditor (container, options, json) {
         'ace', 'theme',
         'ajv', 'schema',
         'onChange', 'onEditable', 'onError', 'onModeChange',
-        'escapeUnicode', 'history', 'search', 'mode', 'modes', 'name', 'indentation'
+        'escapeUnicode', 'history', 'search', 'mode', 'modes', 'name', 'indentation', 'lang'
       ];
 
       Object.keys(options).forEach(function (option) {
