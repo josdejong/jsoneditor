41821ef825a595b95be6a8693bd13326e8f1ffc1
jos <wjosdejong@gmail.com>
2016-08-19 21:14:09 +0200

    The factory function now creates a neat wrapper object instead of returning a TreeMode object

--- src/index.js
+++ src/index.js
@@ -4,17 +4,45 @@ import TreeMode from './TreeMode'
 import '!style!css!less!./jsoneditor.less'
 
 /**
- * Factory function to create a new JSONEditor
- * @param container
+ * Create a new json editor
+ * @param {HTMLElement} container
  * @param {Options} options
- * @return {*}
+ * @return {Object}
  * @constructor
  */
 function jsoneditor (container, options) {
-  // TODO: use JSONEditor instead of TreeMode
   const elem = render(h(TreeMode, {options}), container)
-  return elem._component
+  const component = elem._component
+
+  return {
+    isJSONEditor: true,
+
+    _container: container,
+    _options: options,
+    _component: component,
+
+    /**
+     * Set JSON object in editor
+     * @param {Object | Array | string | number | boolean | null} json JSON data
+     * @param {SetOptions} [options]
+     */
+    set: function (json, options = {}) {
+      component.set(json, options)
+    },
+
+    /**
+     * Get JSON from the editor
+     * @returns {Object | Array | string | number | boolean | null} json
+     */
+    get: function () {
+      return component.get()
+    }
+
+    // TODO: implement getText
+    // TODO: implement setText
+    // TODO: implement expand
+  }
 }
 
 // TODO: use export default jsoneditor, doesn't work out of the box in webpack
-module.exports = jsoneditor;
+module.exports = jsoneditor
