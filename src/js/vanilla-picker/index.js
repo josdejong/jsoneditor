ed9a6d48f3ab89a6238f2949ce14e38b72eed666
jos <wjosdejong@gmail.com>
2018-08-29 10:53:33 +0200

    Fixed color picker not working in ES6 projects

--- src/js/vanilla-picker/index.js
+++ src/js/vanilla-picker/index.js
@@ -1,13 +1,16 @@
-var VanillaPicker
+var VanillaPicker;
 
 if (window.Picker) {
   // use the already loaded instance of VanillaPicker
-  VanillaPicker = window.Picker
+  VanillaPicker = window.Picker;
 }
 else {
   try {
-    // load brace
-    VanillaPicker = require('vanilla-picker');
+    // load color picker
+    // Note that we load the ES5 distribution bundle
+    // instead of the "default" as the default currently
+    // points to `src/picker.js` which is ES6 code (v2.3.0).
+    VanillaPicker = require('vanilla-picker/dist/vanilla-picker');
   }
   catch (err) {
     // probably running the minimalist bundle
