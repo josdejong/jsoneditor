74272397b4c310903978e99f3936a4905253d812
RobAley <rob@active-net.co.uk>
2019-02-25 16:03:25 +0000

    change oncreatemenu path to object

--- src/js/treemode.js
+++ src/js/treemode.js
@@ -1695,7 +1695,7 @@ treemode.showContextMenu = function (anchor, onClose) {
   });
   
   if (this.editor.options.onCreateMenu) {
-		items = this.editor.options.onCreateMenu(items, node.getPath());
+		items = this.editor.options.onCreateMenu(items, { path : node.getPath() });
 	}
 
   var menu = new ContextMenu(items, {close: onClose});
