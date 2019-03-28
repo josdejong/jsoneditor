josdejong/jsoneditor

commit 3e7e1cebfd237ed7a41f5f24cabd84b6db8fd625

Fixed #676: JSON Paths containing array properties with a `]` not parsed correctly

src/js/util.js
@@ -742,43 +742,75 @@ exports.isChildOf = function (elem, parent) {
  * @return {Array}
  */
 exports.parsePath = function parsePath(jsonPath) {
-  var prop, remainder;
+  var path = [];
+  var i = 0;
 
-  if (jsonPath.length === 0) {
-    return [];
-  }
-
-  // find a match like '.prop'
-  var match = jsonPath.match(/^\.([\w$]+)/);
-  if (match) {
-    prop = match[1];
-    remainder = jsonPath.substr(prop.length + 1);
-  }
-  else if (jsonPath[0] === '[') {
-    // find a match like
-    var end = jsonPath.indexOf(']');
-    if (end === -1) {
-      throw new SyntaxError('Character ] expected in path');
-    }
-    if (end === 1) {
-      throw new SyntaxError('Index expected after [');
+  function parseProperty () {
+    var prop = ''
+    while (jsonPath[i] !== undefined && /[\w$]/.test(jsonPath[i])) {
+      prop += jsonPath[i];
+      i++;
     }
 
-    var value = jsonPath.substring(1, end);
-    if (value[0] === '\'') {
-      // ajv produces string prop names with single quotes, so we need
-      // to reformat them into valid double-quoted JSON strings
-      value = '\"' + value.substring(1, value.length - 1) + '\"';
+    if (prop === '') {
+      throw new Error('Invalid JSON path: property name expected at index ' + i);
     }
 
-    prop = value === '*' ? value : JSON.parse(value); // parse string and number
-    remainder = jsonPath.substr(end + 1);
-  }
-  else {
-    throw new SyntaxError('Failed to parse path');
+    return prop;
   }
 
-  return [prop].concat(parsePath(remainder))
+  function parseIndex (end) {
+    var name = ''
+    while (jsonPath[i] !== undefined && jsonPath[i] !== end) {
+      name += jsonPath[i];
+      i++;
+    }
+
+    if (jsonPath[i] !== end) {
+      throw new Error('Invalid JSON path: unexpected end, character ' + end + ' expected')
+    }
+
+    return name;
+  }
+
+  while (jsonPath[i] !== undefined) {
+    if (jsonPath[i] === '.') {
+      i++;
+      path.push(parseProperty());
+    }
+    else if (i > 0 && jsonPath[i] === '[') {
+      i++;
+
+      if (jsonPath[i] === '\'' || jsonPath[i] === '"') {
+        var end = jsonPath[i]
+        i++;
+
+        path.push(parseIndex(end));
+
+        if (jsonPath[i] !== end) {
+          throw new Error('Invalid JSON path: closing quote \' expected at index ' + i)
+        }
+        i++;
+      }
+      else {
+        var index = parseIndex(']').trim()
+        if (index.length === 0) {
+          throw new Error('Invalid JSON path: array value expected at index ' + i)
+        }
+        path.push(index);
+      }
+
+      if (jsonPath[i] !== ']') {
+        throw new Error('Invalid JSON path: closing bracket ] expected at index ' + i)
+      }
+      i++;
+    }
+    else {
+      throw new Error('Invalid JSON path: unexpected character "' + jsonPath[i] + '" at index ' + i);
+    }
+  }
+
+  return path;
 };
 
 /**

test/util.test.js
@@ -105,16 +105,19 @@ describe('util', function () {
       assert.deepEqual(util.parsePath('.foo[2].bar'), ['foo', 2, 'bar']);
       assert.deepEqual(util.parsePath('.foo["prop with spaces"]'), ['foo', 'prop with spaces']);
       assert.deepEqual(util.parsePath('.foo[\'prop with single quotes as outputted by ajv library\']'), ['foo', 'prop with single quotes as outputted by ajv library']);
+      assert.deepEqual(util.parsePath('.foo["prop with . dot"]'), ['foo', 'prop with . dot']);
+      assert.deepEqual(util.parsePath('.foo["prop with ] character"]'), ['foo', 'prop with ] character']);
       assert.deepEqual(util.parsePath('.foo[*].bar'), ['foo', '*', 'bar']);
     });
 
     it ('should throw an exception in case of an invalid path', function () {
-      assert.throws(function () {util.parsePath('.')}, /Error/);
-      assert.throws(function () {util.parsePath('[')}, /Error/);
-      assert.throws(function () {util.parsePath('[]')}, /Error/);
-      assert.throws(function () {util.parsePath('.[]')}, /Error/);
-      assert.throws(function () {util.parsePath('["23]')}, /Error/);
-      assert.throws(function () {util.parsePath('.foo bar')}, /Error/);
+      assert.throws(function () {util.parsePath('.')}, /Invalid JSON path: property name expected at index 1/);
+      assert.throws(function () {util.parsePath('[')}, /Invalid JSON path: unexpected character "\[" at index 0/);
+      assert.throws(function () {util.parsePath('[]')}, /Invalid JSON path: unexpected character "\[" at index 0/);
+      assert.throws(function () {util.parsePath('.foo[  ]')}, /Invalid JSON path: array value expected at index 7/);
+      assert.throws(function () {util.parsePath('.[]')}, /Invalid JSON path: property name expected at index 1/);
+      assert.throws(function () {util.parsePath('["23]')}, /Invalid JSON path: unexpected character "\[" at index 0/);
+      assert.throws(function () {util.parsePath('.foo bar')}, /Invalid JSON path: unexpected character " " at index 4/);
     });
 
   });

src/js/polyfills.js
@@ -43,3 +43,10 @@ if (!Array.prototype.find) {
     }
   }
 }
+
+// Polyfill for String.trim
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  };
+}

commit 09ab92017c0247e08d0fba1f891add68dd7eb023

Fix JSON path parsing for array indices (#679)
* Switch JSON path tests to strict equality

The tests for `stringifyPath` and `parsePath` were using
[`assert.deepEqual`](https://nodejs.org/api/assert.html#assert_assert_deepequal_actual_expected_message),
which was causing the tests to pass when they should not have. Beyond
that, the `deepEqual` method is deprecated. The suggested replacement is
[`assert.deepStrictEqual`](https://nodejs.org/api/assert.html#assert_assert_deepstrictequal_actual_expected_message),
which causes the tests to fail where they should.

The difference between the two methods is the different between `==` and
`===`. `deepEqual` would coerce the input `"2"` to `2`, so the test was
passing even though the output was of the wrong type (string instead of
number).

* Coerce numeric indices to numbers in parsePath

This fixes a regression introduced in
3e7e1cebfd237ed7a41f5f24cabd84b6db8fd625.

src/js/util.js
@@ -797,6 +797,8 @@ exports.parsePath = function parsePath(jsonPath) {
         if (index.length === 0) {
           throw new Error('Invalid JSON path: array value expected at index ' + i)
         }
+        // Coerce numeric indices to numbers, but ignore star
+        index = index === '*' ? index : JSON.parse(index);
         path.push(index);
       }
 

test/util.test.js
@@ -98,27 +98,27 @@ describe('util', function () {
   describe('jsonPath', function () {
 
     it('should stringify an array of paths', function() {
-      assert.deepEqual(util.stringifyPath([]), '');
-      assert.deepEqual(util.stringifyPath(['foo']), '.foo');
-      assert.deepEqual(util.stringifyPath(['foo', 'bar']), '.foo.bar');
-      assert.deepEqual(util.stringifyPath(['foo', 2]), '.foo[2]');
-      assert.deepEqual(util.stringifyPath(['foo', 2, 'bar']), '.foo[2].bar');
-      assert.deepEqual(util.stringifyPath(['foo', 2, 'bar_baz']), '.foo[2].bar_baz');
-      assert.deepEqual(util.stringifyPath(['foo', 'prop-with-hyphens']), '.foo["prop-with-hyphens"]');
-      assert.deepEqual(util.stringifyPath(['foo', 'prop with spaces']), '.foo["prop with spaces"]');
+      assert.deepStrictEqual(util.stringifyPath([]), '');
+      assert.deepStrictEqual(util.stringifyPath(['foo']), '.foo');
+      assert.deepStrictEqual(util.stringifyPath(['foo', 'bar']), '.foo.bar');
+      assert.deepStrictEqual(util.stringifyPath(['foo', 2]), '.foo[2]');
+      assert.deepStrictEqual(util.stringifyPath(['foo', 2, 'bar']), '.foo[2].bar');
+      assert.deepStrictEqual(util.stringifyPath(['foo', 2, 'bar_baz']), '.foo[2].bar_baz');
+      assert.deepStrictEqual(util.stringifyPath(['foo', 'prop-with-hyphens']), '.foo["prop-with-hyphens"]');
+      assert.deepStrictEqual(util.stringifyPath(['foo', 'prop with spaces']), '.foo["prop with spaces"]');
     })
 
     it ('should parse a json path', function () {
-      assert.deepEqual(util.parsePath(''), []);
-      assert.deepEqual(util.parsePath('.foo'), ['foo']);
-      assert.deepEqual(util.parsePath('.foo.bar'), ['foo', 'bar']);
-      assert.deepEqual(util.parsePath('.foo[2]'), ['foo', 2]);
-      assert.deepEqual(util.parsePath('.foo[2].bar'), ['foo', 2, 'bar']);
-      assert.deepEqual(util.parsePath('.foo["prop with spaces"]'), ['foo', 'prop with spaces']);
-      assert.deepEqual(util.parsePath('.foo[\'prop with single quotes as outputted by ajv library\']'), ['foo', 'prop with single quotes as outputted by ajv library']);
-      assert.deepEqual(util.parsePath('.foo["prop with . dot"]'), ['foo', 'prop with . dot']);
-      assert.deepEqual(util.parsePath('.foo["prop with ] character"]'), ['foo', 'prop with ] character']);
-      assert.deepEqual(util.parsePath('.foo[*].bar'), ['foo', '*', 'bar']);
+      assert.deepStrictEqual(util.parsePath(''), []);
+      assert.deepStrictEqual(util.parsePath('.foo'), ['foo']);
+      assert.deepStrictEqual(util.parsePath('.foo.bar'), ['foo', 'bar']);
+      assert.deepStrictEqual(util.parsePath('.foo[2]'), ['foo', 2]);
+      assert.deepStrictEqual(util.parsePath('.foo[2].bar'), ['foo', 2, 'bar']);
+      assert.deepStrictEqual(util.parsePath('.foo["prop with spaces"]'), ['foo', 'prop with spaces']);
+      assert.deepStrictEqual(util.parsePath('.foo[\'prop with single quotes as outputted by ajv library\']'), ['foo', 'prop with single quotes as outputted by ajv library']);
+      assert.deepStrictEqual(util.parsePath('.foo["prop with . dot"]'), ['foo', 'prop with . dot']);
+      assert.deepStrictEqual(util.parsePath('.foo["prop with ] character"]'), ['foo', 'prop with ] character']);
+      assert.deepStrictEqual(util.parsePath('.foo[*].bar'), ['foo', '*', 'bar']);
     });
 
     it ('should throw an exception in case of an invalid path', function () {