81b56fe6bf0b2c246c702fcd42e3af03d56240d8
Meir Rotstein <meir@rotstein.co.il>
2022-04-20 16:42:22 +0300

    refactor suggessions process to enable schames with recursicve refs

--- src/js/SchemaTextCompleter.js
+++ src/js/SchemaTextCompleter.js
@@ -7,7 +7,7 @@ import {
 } from './util'
 
 /**
- * SchemaTextCompleter is a completer object that implements the 
+ * SchemaTextCompleter is a completer object that implements the
  * ace ext-language_tools completer API, and suggests completions for the text editor
  * according to the cursor position and the json schema
  */
@@ -16,101 +16,102 @@ export class SchemaTextCompleter {
     this.schema = schema;
     this.schemaRefs = schemaRefs || {};
     this.suggestions = {};
-    this._buildSuggestions(this.schema);
-    setTimeout(() => console.log('TEMP collected paths', this.suggestions, Object.keys(this.suggestions).length), 20000);
+    this.suggestionsRefs = {};
+    this._buildSuggestions();
   }
 
   _buildSuggestions () {
-    this._handleSchemaEntry("" , this.schema);
-  }
-
-  _handleRef(currectPath, refName) {
-    if (this.schemaRefs[refName]) {
-      setTimeout(() => {
-        this._handleSchemaEntry(currectPath, this.schemaRefs[refName]);
-      })
+    this._handleSchemaEntry("" , this.schema, this.suggestions);
+    for (let refName in this.schemaRefs) {
+      this.suggestionsRefs[refName] = {};
+      this._handleSchemaEntry("" , this.schemaRefs[refName], this.suggestionsRefs[refName]);
     }
   }
 
-  _handleSchemaEntry(currectPath, schemaNode) {
+  _handleRef(currectPath, refName, suggestionsObj) {
+    suggestionsObj[currectPath] = suggestionsObj[currectPath] || {}
+    suggestionsObj[currectPath].refs = suggestionsObj[currectPath].refs || []
+    suggestionsObj[currectPath].refs = [...new Set(suggestionsObj[currectPath].refs.concat([refName]))]
+  }
+
+  _handleSchemaEntry(currectPath, schemaNode, suggestionsObj) {
     if(!schemaNode) {
       console.log('wrong',currectPath);
       return;
     }
     if (schemaNode.$ref) {
-      this._handleRef(currectPath, schemaNode.$ref);
+      this._handleRef(currectPath, schemaNode.$ref, suggestionsObj);
       return;
     }
     const ofConditionEntry = this._checkOfConditon(schemaNode);
     if (ofConditionEntry) {
-      this._handleOfCondition(currectPath, schemaNode[ofConditionEntry]);
+      this._handleOfCondition(currectPath, schemaNode[ofConditionEntry], suggestionsObj);
       return;
     }
     switch(schemaNode.type) {
       case 'object':
-        this._handleObject(currectPath, schemaNode)
+        this._handleObject(currectPath, schemaNode, suggestionsObj)
         break;
       case 'string':
       case 'number':
       case 'integer':
-        this._handlePrimitive(currectPath, schemaNode)
+        this._handlePrimitive(currectPath, schemaNode, suggestionsObj)
         break;
       case 'boolean':
-        this._handleBoolean(currectPath, schemaNode)
+        this._handleBoolean(currectPath, schemaNode, suggestionsObj)
         break
       case 'array':
-        this._handleArray(currectPath, schemaNode)
+        this._handleArray(currectPath, schemaNode, suggestionsObj)
     }
   }
 
-  _handleObject(currectPath, schemaNode) {
+  _handleObject(currectPath, schemaNode, suggestionsObj) {
     if (isObject(schemaNode.properties)) {
       const props = Object.keys(schemaNode.properties);
-      this.suggestions[currectPath] = this.suggestions[currectPath] || {};
-      this.suggestions[currectPath].props = this.suggestions[currectPath].props || [];
-      this.suggestions[currectPath].props = [...new Set(this.suggestions[currectPath].props.concat(props))]
+      suggestionsObj[currectPath] = suggestionsObj[currectPath] || {};
+      suggestionsObj[currectPath].props = suggestionsObj[currectPath].props || [];
+      suggestionsObj[currectPath].props = [...new Set(suggestionsObj[currectPath].props.concat(props))]
       props.forEach((prop) => {
         setTimeout(() => {
-          this._handleSchemaEntry(`${currectPath}/${prop}`,schemaNode.properties[prop]);
+          this._handleSchemaEntry(`${currectPath}/${prop}`,schemaNode.properties[prop], suggestionsObj);
         })
       })
     }
   }
 
-  _handlePrimitive(currectPath, schemaNode) {
+  _handlePrimitive(currectPath, schemaNode, suggestionsObj) {
+    suggestionsObj[currectPath] = suggestionsObj[currectPath] || {};
     if (isArray(schemaNode.examples)) {
-      this.suggestions[currectPath] = this.suggestions[currectPath] || {};
-      this.suggestions[currectPath].examples = this.suggestions[currectPath].examples || [];
-      this.suggestions[currectPath].examples = [...new Set(this.suggestions[currectPath].examples.concat(schemaNode.examples))]
+      suggestionsObj[currectPath].examples = suggestionsObj[currectPath].examples || [];
+      suggestionsObj[currectPath].examples = [...new Set(suggestionsObj[currectPath].examples.concat(schemaNode.examples))]
     }
     if (isArray(schemaNode.enum)) {
-      this.suggestions[currectPath] = this.suggestions[currectPath] || {};
-      this.suggestions[currectPath].enum = this.suggestions[currectPath].enum || [];
-      this.suggestions[currectPath].enum = [...new Set(this.suggestions[currectPath].enum.concat(schemaNode.enum))]
+      suggestionsObj[currectPath].enum = suggestionsObj[currectPath].enum || [];
+      suggestionsObj[currectPath].enum = [...new Set(suggestionsObj[currectPath].enum.concat(schemaNode.enum))]
     }
   }
 
-  _handleBoolean(currectPath, schemaNode) {
-    if (!this.suggestions[currectPath]) {
-      this.suggestions[currectPath] = {
+  _handleBoolean(currectPath, schemaNode, suggestionsObj) {
+    if (!suggestionsObj[currectPath]) {
+      suggestionsObj[currectPath] = {
         bool: [true,false]
       }
     }
   }
 
-  _handleArray(currectPath, schemaNode) {
+  _handleArray(currectPath, schemaNode, suggestionsObj) {
     if (schemaNode.items) {
       setTimeout(() => {
-        this._handleSchemaEntry(`${currectPath}/\\d+`, schemaNode.items);
+        this._handleSchemaEntry(`${currectPath}/\\d+`, schemaNode.items, suggestionsObj);
       })
     }
   }
 
-  _handleOfCondition(currectPath, schemaNode) {
+  _handleOfCondition(currectPath, schemaNode, suggestionsObj) {
     if (schemaNode && schemaNode.length) {
       schemaNode.forEach(schemaEntry => {
         setTimeout(() => {
-          this._handleSchemaEntry(currectPath, schemaEntry);
+          this._handleSchemaEntry(currectPath, schemaEntry, suggestionsObj);
         })
       })
     }
@@ -169,30 +170,60 @@ export class SchemaTextCompleter {
       }
       Object.keys(pointers).forEach((ptr) => {
         setTimeout(() => {
+          const matchPointersToPath = (pointer, currentSuggestions, path) => {
+            const option = Object.keys(currentSuggestions).reduce((last, key) => {
+              if(new RegExp(`\^${path}${key}`).test(pointer)) {
+                if (!last || last.length < key.length) {
+                  return key;
+                }
+              }
+              return last;
+            });
+            if (typeof option === 'string') {
+              if (currentSuggestions[option]?.refs?.length) {
+                const mergedSuggestions = {};
+                for (let idx in currentSuggestions[option].refs) {                  
+                  const refName = currentSuggestions[option].refs[idx]                 
+                  if (this.suggestionsRefs[refName]) {
+                    const refSuggestion = matchPointersToPath(pointer, this.suggestionsRefs[refName], `${path}${option}`);
+                    if(refSuggestion?.enum) {
+                      mergedSuggestions.enum = (mergedSuggestions.enum || []).concat(refSuggestion.enum);
+                    }
+                    if(refSuggestion?.examples) {
+                      mergedSuggestions.examples = (mergedSuggestions.examples || []).concat(refSuggestion.examples);
+                    }
+                    if(refSuggestion?.bool) {
+                      mergedSuggestions.bool = (mergedSuggestions.bool || []).concat(refSuggestion.bool);
+                    }
+                    if(refSuggestion?.props) {
+                      mergedSuggestions.props = (mergedSuggestions.props || []).concat(refSuggestion.props);
+                    }
+                  }
+                }
+                return mergedSuggestions;
+              } else if (new RegExp(`\^${path}${option}$`).test(pointer)) {
+                console.log('Text suggession match', {path: pointer, schemaPath: `${path}${option}`, suggestions: currentSuggestions[option]});
+                return currentSuggestions[option];
+              }
+            }
+          }
+          let selectedPtr;
           if (pointers[ptr].key?.line === pos.row) {
             if (pos.column >= pointers[ptr].key.column && pos.column <= pointers[ptr].keyEnd.column) {
-              const parentPtr = ptr.slice(0, ptr.lastIndexOf('/'));
-              const options = Object.keys(this.suggestions).filter(key => {
-                return new RegExp(`\^${key}\$`).test(parentPtr)
-              });
-              if (options.length) {
-                processCompletionsCallback(this.suggestions[options[0]]);
-                return;
-              }
+              selectedPtr = ptr.slice(0, ptr.lastIndexOf('/'));
             }
           } 
           if (pointers[ptr].value?.line === pos.row && 
               pointers[ptr].value?.line === pointers[ptr].valueEnd?.line) { // multiline values are objects
             if (pos.column >= pointers[ptr].value.column && pos.column <= pointers[ptr].valueEnd.column) {
-              const options = Object.keys(this.suggestions).filter(key => {
-                return new RegExp(`\^${key}\$`).test(ptr)
-              });
-              if (options.length) {
-                processCompletionsCallback(this.suggestions[options[0]]);
-                return;
-              }
+              selectedPtr = ptr;
             }
           }
+          if (selectedPtr) {
+            const chosenCompletions = matchPointersToPath(selectedPtr, this.suggestions, '');
+            processCompletionsCallback(chosenCompletions);
+          }
+
         })
       })
     } catch (e) {
