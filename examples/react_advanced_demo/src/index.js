28e36d094b47d1228a5103d8346533038bdc5b2b
Jos de Jong <wjosdejong@gmail.com>
2022-10-28 17:35:54 +0200

    Update dependencies of React examples

--- examples/react_advanced_demo/src/index.js
+++ examples/react_advanced_demo/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />);
